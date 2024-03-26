0f0bfb5c3929acff5fdf300a6fb73b9955b00fa8
nachool <nachma.olivia1394@gmail.com>
2024-03-26 17:48:53 +0700

    membuat opsi pada variabel piluhan

--- 048_FungsiSwitch/048_FungsiSwitch.cpp
+++ 048_FungsiSwitch/048_FungsiSwitch.cpp
@@ -33,9 +33,23 @@ float pembagian(float a, float b)
 }
 
 
-
 int main()
 {
-    
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan : " << endl;
+        cin >> pilihan;
+
+        
+    } 
 }
 
